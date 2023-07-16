c5b005f418585f9dba8d5467d3a2af4a8a65615e
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-07-16 17:35:12 +0530

    books_dumb.js

---
+++ src/books_dumb.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        "bookstitle": "Unlocking Android: A Developer's Guide",
+        "isbn": "1933988673",
+        "sellers":"goyal brother",
+        "customers": "shivam singh"
+       
+    }
+   
+]
